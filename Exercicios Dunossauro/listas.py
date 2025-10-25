810c7bbfd083b39d1227bdc841d4b6b04c86fa20
diogo <alvarwngadiogo228@gmail.com>
2025-10-25 16:33:46 -0700

    commit

---
+++ Exercicios Dunossauro/listas.py
@@ -0,0 +1,126 @@
+
+#Exercicio 1
+
+i = 0
+vetor = []
+for i in range(5):
+    vetor.append(int(input()))
+print(vetor)
+
+
+#Exercicio 2
+
+
+i = 0
+vetor = []
+for i in range(10):
+    vetor.append(int(input()))
+    
+i2 = 9
+while i2>=0:
+    print(vetor[i2])
+    i2 =i2 -1
+
+
+#Exercicio 3
+
+
+vetor = []
+i = 0
+for i in range(4):
+    vetor.append(int(input()))
+    
+i2=0    
+media = 0
+for i2 in range(4):
+    media = media + vetor[i2]
+media = media /4
+print(media)
+
+
+#Exercicio 4
+
+
+vetor = []
+i =0
+for i in range(10):
+    vetor.append(input().lower())
+
+i2=0
+q = 0
+for i2 in range(10):
+    if vetor[i2] != "a" and vetor[i2]!= "e" and vetor[i2]!= "i" and vetor[i2] != "o" and vetor != "u":
+        q = q+1   
+    
+print(q)
+
+
+#Exercicio 5
+
+
+vetor =[]
+i = 0
+
+for i in range(20):
+    vetor.append(int(input()))
+
+i2 =0
+impar = []
+par = []
+
+for i2 in range(20):
+    if vetor[i2] %2 ==0:
+        par.append(vetor[i2])
+    else:
+        impar.append(vetor[i2])
+        
+print(vetor)
+print(impar)
+print(par)
+
+
+#Exercicio 6
+
+
+vetor =[]
+i =0
+n1 =0
+n2=0
+n3 =0
+n4= 0
+quantidadeAluno =0
+for i in range(10):
+    n1=float(input())
+    n2=float(input())
+    n3=float(input())
+    n4=float(input())
+    
+    media = (n1+n2+n3+n4)/4
+    if media >=7:
+        vetor.append(media)
+        quantidadeAluno = quantidadeAluno+1
+        
+print(vetor)
+print(quantidadeAluno)
+
+
+#Exercicio 7
+
+
+vetor =[]
+
+for _ in range(5):
+    vetor.append(int(input()))
+
+i=0
+soma=0
+for i in range(5):
+    soma += vetor[i]
+print(soma)
+
+mult=1
+for i in range(5):
+    mult *= vetor[i]
+print(mult)
+
+
